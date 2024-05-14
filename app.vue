3ec629ec4ea6a3c088f112708ec2afbd1eef2f1b
ogios <2134692955@qq.com>
2024-05-14 12:15:12 +0800

    first

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const { data } = await useAsyncData("test", () =>
+  queryContent("/test").findOne(),
+);
+console.log(data);
+</script>
+
+<template>
+  <div>{{ data }}</div>
+</template>
